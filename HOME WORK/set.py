4cfd79df12bdb88545d16e47fa2b1bdde6e2adfc
diinnnaaa <149644597+diinnnaaa@users.noreply.github.com>
2023-11-16 08:27:26 +0600

    Create set.py

---
+++ HOME WORK/set.py
@@ -0,0 +1,115 @@
+#дубликаттар жок
+#Реты жок
+#{}
+#коп елементтты коссак update if one word use add
+#remove элкментты алады
+# pop рандомайзер и убирает
+# cities = {'Astana', 'Almaty','Karaganda','Petropavl','Kokshetau'}
+# print(len(cities))
+# new_cit = input("Enter a city: ")
+# cities.add(new_cit)
+# print (cities)
+
+# cities.add('Mangystau')
+# print(len(cities))
+
+
+# new_cit = input("Enter a city: ")
+# if new_cit in cities:
+#     cities.remove(new_cit)
+#     print(f"{new_cit} was removed from the set ")
+# else:
+#     print("No such city")
+
+
+# cities= {"almaty", "Astana", "Aktau"}
+# cit = input("City")
+# p = cit.split()
+# cities.update(p)
+# print(cities)
+
+
+
+#--------------1
+# it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+# print(len(it_companies))
+
+#--------------2pflfybt
+# it_companies.add('twitter')
+# print(it_companies)
+
+#----------------3egh
+# it_companies.update(['vk', 'agent', 'mailru'])
+# print(it_companies)
+
+#----------------4egh
+# it_companies.remove('Google')
+# print(it_companies)
+
+#----------------5egh
+# ничем
+
+#------------------lvl 2
+
+#--------------------1 egh
+# A = {19, 22, 24, 20, 25, 26}
+# B = {19, 22, 20, 25, 26, 24, 28, 27}
+# C = A.union(B)
+# print(C)
+
+#--------------------2egh
+# A = {19, 22, 24, 20, 25, 26}
+# B = {19, 22, 20, 25, 26, 24, 28, 27}
+# C = A.intersection(B)
+# print(C)
+
+# --------------------3egh
+# A = {19, 22, 24, 20, 25, 26}
+# B = {19, 22, 20, 25, 26, 24, 28, 27}
+# C = B.issubset(A)
+# print(C)
+
+# --------------------4egh
+# A = {19, 22, 24, 20, 25, 26}
+# B = {30,31}
+# C = B.isdisjoint(A)
+# print(C)
+
+# ---------------------5egh
+# A = {19, 22, 24, 20, 25, 26}
+# B = {30,31}
+# C = A.union(B)
+# D = B.union(A)
+# print(C)
+# print(D)
+
+#------------------------6egh
+# A = {19, 22, 24, 20, 25, 26,}
+# B = {30,31}
+# C = A.symmetric_difference(B)
+# print(C)
+
+#-------------------------7egh
+# A = {19, 22, 24, 20, 25, 26,}
+# B = {30,31}
+# del B
+# del A
+# -----------------------lvl3
+#------------------------1egh
+# age = [22, 19, 24, 25, 26, 24, 25, 24]
+# sets = set(age)
+# len_list = len(age)
+# len_set = len(sets)
+# print("List length:", len_list)
+# print("Set length:", len_set)
+
+#------------------------2egh
+# String is a collection of alphabets, words or other characters.
+# A list is an ordered, mutable collection of elements.
+# A tuple is an ordered, immutable collection of elements.
+# A set-in this list the elements are not repeated and they do not have a specific order
+
+#-------------------------3egh
+# text = 'I am a teacher and I love to inspire and teach people.'
+# s = set(text.split())
+# print(s)
